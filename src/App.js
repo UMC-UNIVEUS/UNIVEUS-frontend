27471c543a6aeb6fecf862890ace874178d574fc
soi-ha <soy2302ten@gmail.com>
2023-06-21 17:13:47 +0900

    MainPage 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import { Reset } from 'styled-reset';
 import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
 			<Footer />
+			<MainPage />
 		</div>
 	);
 }
