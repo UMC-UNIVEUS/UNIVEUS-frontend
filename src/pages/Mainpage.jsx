84fe29e553f18a1c30ea9290ff4da4c92304259d
soi-ha <soy2302ten@gmail.com>
2023-06-21 18:03:06 +0900

    Mainpage 생성

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,13 @@
+import TopBar from '../components/TopBar';
+import Footer from '../components/Footer';
+
+const MainPage = () => {
+	return (
+		<div id="MainPage">
+			<TopBar />
+			<Footer />
+		</div>
+	);
+};
+
+export default MainPage;
