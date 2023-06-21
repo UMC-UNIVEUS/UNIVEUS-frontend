f4c2f592702ad0260700e0b02babd7b71e4f44b6
soi-ha <soy2302ten@gmail.com>
2023-06-21 17:07:10 +0900

    footer 컴포넌트 완성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
 import { Reset } from 'styled-reset';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<Footer />
 		</div>
 	);
 }
