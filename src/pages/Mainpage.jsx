e21c7c5f12986871e10b3622a46e6ae883c842cc
jungwoo3490 <jungwoo3490@naver.com>
2023-06-28 18:18:17 +0900

    메인페이지 초안 구현 완료(추후 기능 추가 필요)

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,15 +1,20 @@
 import Banner from '../components/Banner';
 import Comment from '../components/Comment';
+import Footer from '../components/Footer';
+import MenuBtn from '../components/MenuBtn';
 import TopBar from '../components/TopBar';
 import CommentList from '../containers/CommentList';
+import PostContainer from '../containers/PostContainer';
 
 const MainPage = () => {
 	return (
 		<div id="MainPage">
-			<TopBar />
-			<Banner />
-			<CommentList />
-		</div>
+            <TopBar />
+            <Banner />
+            <CommentList />
+            <PostContainer />
+            <Footer />
+        </div>
 	);
 };
 
