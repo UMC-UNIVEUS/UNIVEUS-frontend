5fa5c22a76354840e938aba2bcd1649c669b84ad
soi-ha <soy2302ten@gmail.com>
2023-06-29 21:03:53 +0900

    마이페이지, profile img 업로드 보여주기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,49 +1,75 @@
 import { useForm } from 'react-hook-form';
+import { useState, useRef } from 'react';
+import profileLogo from '../assets/images/biglogo.png';
+import '../styles/Profile.scss';
 
 export default function Profile() {
 	const { register, handleSubmit } = useForm();
+	const [imgFile, setImgFile] = useState('');
+	const imgRef = useRef();
+
+	// 이미지 업로드 input의 onChange
+	const saveImgFile = () => {
+		const file = imgRef.current.files[0];
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onloadend = () => {
+			setImgFile(reader.result);
+		};
+	};
 	return (
-		<form onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
+		<form className="profile-form" onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
 			<div className="pro-privacy">
 				<div className="info-right">
-					<div className="right-img"></div>
+					<div className="right-img">
+						<img src={imgFile ? imgFile : profileLogo} alt="프로필 이미지" />
+					</div>
 					<div className="right-btns">
 						<label className="modify-label-btn" htmlFor="mypage-profile-img">
 							프로필 수정
 						</label>
-						<input type="file" accept="image/*" id="mypage-profile-img" className="modify-input" />
+						<input
+							type="file"
+							accept="image/*"
+							id="mypage-profile-img"
+							className="modify-input"
+							onChange={saveImgFile}
+							ref={imgRef}
+						/>
 						<button className="delete-btn">프로필 삭제</button>
 					</div>
 				</div>
 				<hr />
 				<div className="info-left">
-					<div className="left-name">
-						<input type="text" placeholder="닉네임을 입력해주세요." />
-					</div>
+					<label className="left-name-label" htmlFor="mypage-profile-name">
+						닉네임
+					</label>
+					<input
+						type="text"
+						id="mypage-profile-name"
+						className="left-name-input"
+						placeholder="닉네임을 입력해주세요."
+					/>
 					<div className="left-gender">
-						<label for="male">남</label>
-						<input type="radio" id="male" name="drone" value="male" />
-						<label for="female">여</label>
-						<input type="radio" id="female" name="drone" value="female" />
+						<label className="left-gender-label">성별</label>
+						<label htmlFor="mypage-profile-male">남</label>
+						<input type="radio" id="mypage-profile-male" name="drone" value="male" />
+						<label htmlFor="mypage-profile-female">여</label>
+						<input type="radio" id="mypage-profile-female" name="drone" value="female" />
 					</div>
 				</div>
 			</div>
 			<div className="pro-interest">
-				<label htmlFor="interest">관심사</label>
-				<textarea name="interest" id="interest" cols="30" rows="10"></textarea>
+				<label htmlFor="mypage-profile-interest">관심사</label>
+				<textarea name="interest" id="mypage-profile-interest" cols="30" rows="10"></textarea>
 			</div>
 			<div className="pro-selfintro">
-				<label
-					htmlFor="selfintro
-        "
-				>
-					자기소개
-				</label>
-				<textarea name="selfintro" id="selfintro" cols="30" rows="10"></textarea>
+				<label htmlFor="mypage-profile-selfintro">자기소개</label>
+				<textarea name="selfintro" id="mypage-profile-selfintro" cols="30" rows="10"></textarea>
 			</div>
 			<div className="pro-btns">
 				<button className="secession-btn">회원탈퇴</button>
-				<button className="save-btn">저장하기</button>
+				<input className="save-btn" type="submit" value={'저장하기'} />
 			</div>
 		</form>
 	);
