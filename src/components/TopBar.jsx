9ffee863df8fbdf42993d30e70568b82db55d227
jungwoo3490 <jungwoo3490@naver.com>
2023-07-10 17:22:16 +0900

    TopBar 로그인 여부 state 및 검색창 입력데이터 state 추가

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -5,7 +5,23 @@ import miniLogoImage from '../assets/minilogo.png'
 import bellImage from '../assets/bell.png'
 import '../styles/Header.scss'
 
+import { useState } from 'react'
+
 const TopBar = () => {
+
+    const [isLogin, setIsLogin] = useState(false);
+    const [value, setValue] = useState('');
+
+    const toggleIsLogin = () => {
+        setIsLogin(!isLogin);
+    };
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    console.log(value);
+
     return (
         <div className="TopBar">
             <div className="title">
@@ -16,19 +32,26 @@ const TopBar = () => {
                 </div>
             </div>
             <div className="searchbar">    
-                <input type='text' className='searchinput' placeholder='검색할 키워드를 입력해 주세요(제목)'/>
+                <input type='text' className='searchinput' placeholder='검색할 키워드를 입력해 주세요(제목)' onChange={handleChange}/>
                 <img src={navigationArrowImage} className='navigationarrow' />
             </div>
-            <div className="sidemenu">
-                <div className='ticketbox'>
-                    <img src={ticketImage} style={{width: '22px', height: '22px', marginLeft: '8px'}}/>
-                    <div style={{width: '0px', height: '21px', borderLeft: '1px solid #b5b3b3', marginLeft: '9px'}}></div>
-                    <div style={{marginLeft: '8px', fontWeight: '500', fontSize: '16px'}}>2 ticket</div>
+            {isLogin ?
+                <div className="sidemenu">
+                    <div className='ticketbox'>
+                        <img src={ticketImage} style={{width: '22px', height: '22px', marginLeft: '8px'}}/>
+                        <div style={{width: '0px', height: '21px', borderLeft: '1px solid #b5b3b3', marginLeft: '9px'}}></div>
+                        <div style={{marginLeft: '8px', fontWeight: '500', fontSize: '16px'}}>2 ticket</div>
+                    </div>
+                    <img src={miniLogoImage} className='minilogo' />
+                    <img src={bellImage} className='bell'></img>
+                </div> 
+                : 
+                <div className="sidemenu">
+                    <button className='loginbutton' onClick={toggleIsLogin}>
+                        로그인하기
+                    </button>
                 </div>
-                <img src={miniLogoImage} className='minilogo' />
-                <img src={bellImage} className='bell'></img>
-            </div>
-            <div></div>
+            }
         </div>
     );
 };
